3bff95c31d30baeb0c0a88fc672e0fc2c0798ed0
Lutz, Bryan <LUTZB1@nationwide.com>
2018-04-02 09:14:42 -0400

    Refactor ProductFactory. Still needs some work.

--- src/main/java/product/ProductFactory.java
+++ src/main/java/product/ProductFactory.java
@@ -1,19 +1,26 @@
 package main.java.product;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
+
 import main.java.exception.InvalidProductException;
 import main.java.util.Constants;
 
 public class ProductFactory {
+	
+	private static final Map<String, Supplier<IProduct>> products = new HashMap<>();
+		static {
+			products.put(Constants.COLA, Cola::new);
+			products.put(Constants.CANDY, Candy::new);
+			products.put(Constants.CHIPS, Chips::new);
+	  }
 
 	public static IProduct getProduct(String productType) throws InvalidProductException {
-		if (productType == null) {
-			throw new InvalidProductException("Null product type.");
-		} else if (productType.equals(Constants.COLA)) {
-			return new Cola();
-		} else if (productType.equals(Constants.CANDY)) {
-			return new Candy();
-		} else if (productType.equals(Constants.CHIPS)) {
-			return new Chips();
+		Supplier<IProduct> product = products.get(productType);
+		
+		if (product != null) {
+			return product.get();
 		}
 
 		throw new InvalidProductException("Unknown product type.");
